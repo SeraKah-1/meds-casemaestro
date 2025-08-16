ba1271a26e27f60baca9604c3ee461558558ee78
SeraKah-1 <farreladitya38@gmail.com>
2025-08-16 12:57:43 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,15 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ErrorBoundary } from './components/ErrorBoundary';
 
 const el = document.getElementById('root')!;
 createRoot(el).render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>
 );
+
+// (optional) register service worker nanti di Part 11
