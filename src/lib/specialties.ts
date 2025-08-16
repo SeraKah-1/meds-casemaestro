72e2f383100f2c185a60e930616992c886206f68
SeraKah-1 <farreladitya38@gmail.com>
2025-08-16 11:37:03 +0700

    Create specialties.ts

---
+++ src/lib/specialties.ts
@@ -0,0 +1,14 @@
+export const specialties = [
+  'cardiology',
+  'pulmonology',
+  'neurology',
+  'gastroenterology',
+  'endocrinology',
+  'infectious-disease',
+  'nephrology',
+  'hematology',
+  'oncology',
+  'obgyn',
+  'pediatrics',
+  'emergency'
+] as const;
